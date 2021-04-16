0aed5daa0840d3955b9170d2e5e1a00974e54d4e
robmatelyan <robmatelyan@gmail.com>
2021-04-16 01:15:11 -0400

    reading json

---
+++ StarterCode/static/js/app.js
@@ -0,0 +1,18 @@
+//Use the D3 library to read in samples.json
+d3.json("samples.json").then(function(data) {
+    // console.log(data);
+    console.log(data.samples);
+    
+    // grab values from the data
+    var name = data.name;
+    var demographic = data.metadata;
+    var samples = data.samples;
+
+    console.log(samples);
+
+});
+
+// Create a horizontal bar chart with a dropdown menu to display the top 10 OTUs found in that individual
+// Use sample_values as the values for the bar chart
+// Use otu_ids as the labels for the bar chart
+// Use otu_labels as the hovertext for the chart
